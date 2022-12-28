9e6290744857c6c852b8362d645ef71758066354
juanchotello98 <tello.juan@correounivalle.edu.co>
2022-12-28 01:42:19 -0500

    deploy

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-//const express = require('express');
-//const path = require('path');
+const express = require('express');
+const path = require('path');
 
-//const app = express();
+const app = express();
 
-//app.use(express.static('./dist/cinemafront'));
+app.use(express.static('./dist/cinemafront'));
 
-//app.get('/', (req, res) => 
-//    res.sendFile('index.html', {root : 'dist/cinemafront'}),
-//);
+app.get('/*', (req, res) => 
+    res.sendFile('index.html', {root : 'dist/cinemafront'}),
+);
 
-
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-
-const app = express()
-
-app.use('/auth', serveStatic(path.join(__dirname,'/dist')))
-
-const port = process.env.PORT || 8080
-app.listen(port)
+app.listen(process.env.PORT || 8080)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,9 +39,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      useHash:true
-    })
+    RouterModule.forRoot(routes)
   ],
   exports:[
     RouterModule
