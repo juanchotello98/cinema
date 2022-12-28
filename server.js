4d2116e70c8d51ab80c2126df4397c600e4d97fe
juanchotello98 <tello.juan@correounivalle.edu.co>
2022-12-28 01:17:50 -0500

    deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname,'/dist')))
+app.use('/auth', serveStatic(path.join(__dirname,'/dist')))
 
 const port = process.env.PORT || 8080
 app.listen(port)
